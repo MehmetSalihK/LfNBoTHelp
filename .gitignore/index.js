bec32e54040ea734204453960d90d7bf8999de48
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-04-26 21:54:44 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -39,6 +39,32 @@ Client.on("message", async (message) => {
 		.setDescription(message.author.username + " dit: " + args.join(" "));
 		message.channel.send({embed})
 	} else
+		
+	if (command === "cat") {
+	   const { body } = await superagent
+	   .get('aws.random.cat/meow');
+	   const embed = new Discord.RichEmbed()
+	   .setColor(0x954D23)
+	   .setTitle("Meow :cat:")
+	   .setImage(body.file)
+	   message.channel.send({embed})
+   } else
+
+   if (command === "announcement") {
+	   if (message.member.hasPermission("ADMINISTRATOR")) {
+		   const color = args[0]
+				
+		   const text = args.slice(1).join(" ");
+		   if (text.length < 1) return message.channel.send("Can not announce nothing");
+		   //const colour = args.slice(2).join("");
+		   const embed = new Discord.RichEmbed()
+		   .setColor("0x" + color)
+		   .setTitle("Important Announcement:")
+		   .setDescription(text);
+		   message.channel.send("@everyone")
+		   message.channel.send({embed})
+	   }
+   } else
 
 	if (command == "helpadmin") {
 		message.delete (30);
