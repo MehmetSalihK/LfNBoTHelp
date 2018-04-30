6c6ce950483e8ee8f3ab13ad880ea4767cbc9e5b
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-04-30 07:37:35 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,6 +21,7 @@ Client.on("message", async (message) => {
 	} else
 
 	if (command === "say") {
+	   if (message.member.hasPermission("ADMINISTRATOR")) {
 		message.delete()
         const embed = new Discord.RichEmbed()
 		.setColor(0x954D23)
